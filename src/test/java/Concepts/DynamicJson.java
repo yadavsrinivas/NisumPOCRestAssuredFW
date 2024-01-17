413a7b59104c005c5833a5945cb56d82bdc7333c
syadav <Sidiksha@19>
2024-01-17 16:30:21 +0530

    Second commit

--- src/test/java/Concepts/DynamicJson.java
+++ src/test/java/Concepts/DynamicJson.java
@@ -1,5 +1,7 @@
-package Files;
+package Concepts;
 
+import Files.ReUsableMethods;
+import Files.payLoad;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.testng.annotations.DataProvider;
@@ -9,7 +11,7 @@ import static io.restassured.RestAssured.*;
 
 public class DynamicJson {
 
-    @Test(dataProvider = "BooksData")
+/*    @Test(dataProvider = "BooksData")
     public void addBook(String isbn, String aisle){
         RestAssured.baseURI="http://216.10.245.166";
        String response =  given().header("Content-Type", "application/json")
@@ -21,8 +23,22 @@ public class DynamicJson {
         JsonPath js = ReUsableMethods.rawToJson(response);
         String id = js.get("ID");
         System.out.println(id);
-    }
+    }*/
 
+
+    @Test(dataProvider = "BooksData")
+    public void deleteBook(String isbn, String aisle){
+        RestAssured.baseURI="http://216.10.245.166";
+        String response =  given().header("Content-Type", "application/json")
+                .body(payLoad.Addbook(isbn, aisle))
+                .when()
+                .delete("/Library/Addbook.php")
+                .then().log().all().assertThat().statusCode(200)
+                .extract().response().asString();
+        JsonPath js = ReUsableMethods.rawToJson(response);
+      //  String msg = js.getString();
+      //  System.out.println(id);
+    }
 
     @DataProvider(name ="BooksData")
     public Object[][] getData(){
